9702e046ff9b90f3b94f79d00838f5bfd3c34fa7
jguest8226 <jonathanguest@Jonathans-MacBook-Pro.local>
2017-11-03 16:37:50 -0400

    test test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <div>
+      <div class="hello">
+      <h3> Your Weather Projecta Creators:</h3>
+      </div>
+      <ul>
+        <li v-for="creator in creators">
+          {{creator.firstname}} {{creator.lastname}}
+        </li>
+      </ul>
+    </div>
+    <div class="hello">
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+    <li><router-link to="oneDay">{{ onedaymsg }}</router-link></h2></li>
+    <li><router-link to="./HelloWorld">{{ tendaymsg }}</router-link></h2></li>
     </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+    </div>
+    </div>
 </template>
 
 <script>
@@ -25,7 +25,14 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Weather Projecta',
+      onedaymsg: 'Click for your One Day Forecast!',
+      tendaymsg: 'Click for your Ten Day Forecast!',
+      creators: [
+        {firstname: 'Joanthan', lastname: 'Guest'},
+        {firstname: 'Jennifer', lastname: 'Guest'},
+        {firstname: 'Violet', lastname: 'Guest'}
+      ]
     }
   }
 }
